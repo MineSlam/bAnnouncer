e5cc0eefcad4dc773d2694f751587dd35024b780
Rowan <rowanpsmith@yahoo.com.au>
2020-07-15 02:37:40 +1000

    Forgot to register PlaceholderAPIHook

--- src/main/java/mccloskey/callum/bannouncer/Main.java
+++ src/main/java/mccloskey/callum/bannouncer/Main.java
@@ -1,6 +1,7 @@
 package mccloskey.callum.bannouncer;
 
 import mccloskey.callum.bannouncer.cmd.AnnouncerCommand;
+import mccloskey.callum.bannouncer.util.PlaceholderAPIHook;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
@@ -15,6 +16,8 @@ public class Main extends JavaPlugin {
         bAnnouncer = new Announcer();
 
         Objects.requireNonNull(getCommand("bannouncer")).setExecutor(new AnnouncerCommand());
+
+        new PlaceholderAPIHook().register();
     }
 
     public void onDisable() {
